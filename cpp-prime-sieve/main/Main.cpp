ee87ff86e66cf8b076cfb0963462a119706b7ca3
David Zhang <dzhang19@slb.com>
2021-04-01 15:44:20 +0200

    compile on windows

--- cpp-prime-sieve/main/Main.cpp
+++ cpp-prime-sieve/main/Main.cpp
@@ -16,8 +16,13 @@ int main(int argc, char* argv[])
 
     std::string_view argRange{argv[1]};
     int range{0};
+#ifdef _WIN32
+    auto result = std::from_chars(argRange.begin()._Unwrapped(), argRange.end()._Unwrapped(), range);
+    if (result.ptr != argRange.end()._Unwrapped()) {
+#else
     auto result = std::from_chars(argRange.begin(), argRange.end(), range);
-    if(result.ptr != argRange.end()) {
+    if (result.ptr != argRange.end()) {
+#endif
         std::cerr << "invalid range: not an integer (base 10)" << std::endl;
         return 1;
     }
@@ -29,8 +34,13 @@ int main(int argc, char* argv[])
     int iterationLimit{1};
     if(argc == 3) {
         std::string_view argIterations{argv[2]};
+#ifdef _WIN32
+        result = std::from_chars(argIterations.begin()._Unwrapped(), argIterations.end()._Unwrapped(), iterationLimit);
+        if(result.ptr != argIterations.end()._Unwrapped()) {
+#else
         result = std::from_chars(argIterations.begin(), argIterations.end(), iterationLimit);
-        if(result.ptr != argIterations.end()) {
+        if (result.ptr != argIterations.end()) {
+#endif
             std::cerr << "invalid number of iterations: not an integer (base 10)" << std::endl;
             return 1;
         }
