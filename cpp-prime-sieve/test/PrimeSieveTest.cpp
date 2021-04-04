cc78d41e6306e101ce9daeb134a21759a6ad225c
David Zhang <zhngdvd88@gmail.com>
2021-04-04 19:25:59 +0200

    cpp: add some more tests

--- cpp-prime-sieve/test/PrimeSieveTest.cpp
+++ cpp-prime-sieve/test/PrimeSieveTest.cpp
@@ -7,6 +7,48 @@ using namespace zhang::math;
 class PrimeSieveTest : public testing::Test
 { };
 
+TEST_F(PrimeSieveTest, range0)
+{
+    PrimeSieve primeSieve{0};
+    primeSieve.sieve();
+    int numberOfPrimes = primeSieve.countPrimes();
+    EXPECT_EQ(numberOfPrimes, 0);
+    EXPECT_FALSE(primeSieve.isPrime(0));
+}
+
+TEST_F(PrimeSieveTest, range1)
+{
+    PrimeSieve primeSieve{1};
+    primeSieve.sieve();
+    int numberOfPrimes = primeSieve.countPrimes();
+    EXPECT_EQ(numberOfPrimes, 0);
+    EXPECT_FALSE(primeSieve.isPrime(0));
+    EXPECT_FALSE(primeSieve.isPrime(1));
+}
+
+TEST_F(PrimeSieveTest, range2)
+{
+    PrimeSieve primeSieve{2};
+    primeSieve.sieve();
+    int numberOfPrimes = primeSieve.countPrimes();
+    EXPECT_EQ(numberOfPrimes, 1);
+    EXPECT_FALSE(primeSieve.isPrime(0));
+    EXPECT_FALSE(primeSieve.isPrime(1));
+    EXPECT_TRUE(primeSieve.isPrime(2));
+}
+
+TEST_F(PrimeSieveTest, range3)
+{
+    PrimeSieve primeSieve{3};
+    primeSieve.sieve();
+    int numberOfPrimes = primeSieve.countPrimes();
+    EXPECT_EQ(numberOfPrimes, 2);
+    EXPECT_FALSE(primeSieve.isPrime(0));
+    EXPECT_FALSE(primeSieve.isPrime(1));
+    EXPECT_TRUE(primeSieve.isPrime(2));
+    EXPECT_TRUE(primeSieve.isPrime(3));
+}
+
 TEST_F(PrimeSieveTest, range100)
 {
     PrimeSieve primeSieve{100};
